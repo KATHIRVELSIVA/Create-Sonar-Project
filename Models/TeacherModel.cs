ac5b3de5dc04b0a28e30375b72e027310ae00c34
Kathirvel <121964805+KATHIRVELSIVA@users.noreply.github.com>
2024-01-22 11:14:12 +0530

    Update TeacherModel.cs
    Review code

--- Models/TeacherModel.cs
+++ Models/TeacherModel.cs
@@ -4,7 +4,7 @@ public class TeacherModel
 {
     //Teacher Id, Teacher Name, Teacher Email, Teacher Qualification and Teacher Location.
     [Key]
-    public int Id { get; set; }
+    public int id { get; set; }
     public string? Name { get; set; }
     public string? Email { get; set; }
     public string? Qualification { get; set; }
